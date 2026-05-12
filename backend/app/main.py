6cfc84abe2f53d536085477b448e628952c472a2
sdeshariqmirza <shariqmirza139@gmail.com>
2026-05-12 16:29:15 +0530

    fixed cors for netlify

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://astounding-cajeta-fefd31.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
